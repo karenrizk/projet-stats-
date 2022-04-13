f4f8c6d19453bc8a3afa38dee03fecf1a5a57f81
Karen RIZK <karen.rizk@ens.uvsq.fr>
2022-04-13 15:40:20 +0200

    Debut

--- stats.py
+++ stats.py
@@ -1,3 +1,4 @@
+from asyncore import write
 import tkinter as tk 
 import random as rd
 from urllib.parse import ParseResultBytes 
@@ -6,11 +7,19 @@ from urllib.parse import ParseResultBytes
 
 
 
+
+
+
+
+# Outils
 def cree_fichier_alea(nb,nomfichier):
-    pass
+    """nom d'un fichier sur le disque dur, fonction ouvre et lit ce fichier """
+    nbrcloud = open("nbrcloud","w")
+    nbrcloud=write
 
 
 def lit_fichier(nomfic):
+    """fichier contient les coordonnees des point d'un nuage """
     pass
 
 
@@ -22,6 +31,12 @@ def trace_droite(a,b):
     pass
 
 
+
+
+
+
+
+# Calculs statistiques"
 def moyenne(serie):
     pass
 
@@ -45,14 +60,44 @@ def droite_reg(serieX,serieY):
     pass
 
 
+
+
+
+
+
+
+
+#Fonction fenetre graphique 
 def Tracerdroite():
+    pass
+
+def autrecouleur():
+    pass
+
+
+def quitter():
     pass
 
 
 
 
 
+#fenetre graphique
 racine = tk.Tk()
 racine.title("projet stats")
 canvas = tk.Canvas(racine, width=1000, height=1000, bg='black')
 tracerdroite = tk.Button(racine, text="Tracer la droite", command=Tracerdroite)
+Autrecouleur=tk.Button(racine,text="Autre couleur",command=autrecouleur )
+Quitter=tk.Button(racine,text="Quitter", command=quitter)
+
+
+
+
+
+canvas.grid()
+tracerdroite.grid()
+Autrecouleur.grid()
+Quitter.grid()
+
+
+racine.mainloop()
